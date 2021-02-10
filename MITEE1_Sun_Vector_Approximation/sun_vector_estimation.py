3a04b1585c467eb4a664f4188e6e05da2274be69
Brian Leung <brianleung329@gmail.com>
2021-02-09 19:09:24 -0500

    (Sun Vector Approx) Basic program has been implemented
    +The basic sun vector approximation has been implemented. More testing still needs to be done.

--- MITEE1_Sun_Vector_Approximation/sun_vector_estimation.py
+++ MITEE1_Sun_Vector_Approximation/sun_vector_estimation.py
@@ -11,7 +11,7 @@ OUTPUT:
 import numpy as np
 
 def estimate_sun_vec(pd_volts):
-    ###Important Values###
+    ### IMPORTANT VALUES ###
     #True: throw errors, False: return <0,0,1>
     error_vector = np.array([0,0,1])
     throw_error = True
@@ -21,20 +21,19 @@ def estimate_sun_vec(pd_volts):
 
     max_voltage = 1
     min_voltage = 0
-    ###End Important Values###
+    ### END IMPORTANT VALUES ###
     
-    sv_approx = np.zeros((1,3))
     #Scale the pd voltage according to the minimum and maximum voltages. (ideally taken across all days)
     pd_volts = (pd_volts-min_voltage)/(max_voltage-min_voltage)
-    
     #Determines which sensors we should be paying attention to.
     #Find largest pd voltage value.
+
     primary_sensor = np.argmax(pd_volts)
-    #Find second largest pd voltage value.
-    temp = pd_volts
-    temp[primary_sensor] = 0
-    secondary_sensor = np.argmax(pd_volts)
     
+    #Find second largest pd voltage value.
+    temp = np.copy(pd_volts)
+    temp[primary_sensor] = 0
+    secondary_sensor = np.argmax(temp)
     #If both sensors are on opposite sides of the craft, something doesn't seem right. This will handle it.
     if (secondary_sensor == (primary_sensor+2)%4):
         #Change the secondary sensor to one of the sides next to the primary side, trying to make a best guess.
@@ -46,36 +45,48 @@ def estimate_sun_vec(pd_volts):
                 raise ValueError("Brightness is high on opposite sides of the craft.")
             else:
                 return error_vector
+    #print("primary",primary_sensor)
+    #print("secondary",secondary_sensor)
 
     #We can now process this using arccos.
     pd_angles = np.arccos(pd_volts)
     #pd_angles now has the angles of incidence of the 4 sensors in radians.
-    #TL TR BL BR
-    if (primary_sensor==0):
+
+    if (primary_sensor==0): #TOP
         primary_vec = np.array([np.tan(pd_angles[0]),1])
-    if (primary_sensor==1):
+    if (primary_sensor==1): #LEFT
         primary_vec = np.array([-1,np.tan(pd_angles[0])])
-    if (primary_sensor==2):
+    if (primary_sensor==2): #BOTTOM
         primary_vec = np.array([np.tan(pd_angles[0]),-1])
-    if (primary_sensor==3): 
+    if (primary_sensor==3): #RIGHT
         primary_vec = np.array([1,np.tan(pd_angles[0])])
-    if (secondary_sensor==0):
+    if (secondary_sensor==0): #TOP
         secondary_vec = np.array([np.tan(pd_angles[0]),1])
-    if (secondary_sensor==1):
+    if (secondary_sensor==1): #LEFT
         secondary_vec = np.array([-1,np.tan(pd_angles[0])])
-    if (secondary_sensor==2):
+    if (secondary_sensor==2): #BOTTOM
         secondary_vec = np.array([np.tan(pd_angles[0]),-1])
-    if (secondary_sensor==3): 
+    if (secondary_sensor==3): #RIGHT
         secondary_vec = np.array([1,np.tan(pd_angles[0])])
-    if (secondary_sensor%2==0):
-        
-    secondary_unit_vec = secondary_vec / (secondary_vec**2).sum()**0.5
-    print(pd_angles)
+
+    if (primary_sensor%2==0): #if the primary vector is top/bottom and secondary vector is left/right
+        primary_vec[0] = primary_vec[0]*secondary_vec[0]
+        secondary_vec[1] = secondary_vec[1]*primary_vec[1]
+    else: #if the secondary vector is top/bottom and primary vector is left/right
+        primary_vec[1] = primary_vec[1]*secondary_vec[1]
+        secondary_vec[0] = secondary_vec[0]*primary_vec[0]
+
+    primary_unit_vec = primary_vec / ((primary_vec**2).sum()**0.5)
+    secondary_unit_vec = secondary_vec / ((secondary_vec**2).sum()**0.5)
+    approx = primary_unit_vec+secondary_unit_vec
+    approx = approx / ((approx**2).sum()**0.5)
+    return np.array([approx[0],approx[1],0])
 
 def main():
-    pudding = np.array([0.56,0.34,0.2,0.1])
-    45pudding = np.array([0.7071,0,0,0.7071])
-    estimate_sun_vec(pudding)
+    #pudding = np.array([0.56,0.34,0.2,0.1])
+    pudding45 = np.array([0.7071,0.7071,0,0])
+    pudding90 = np.array([1,0,0,0])
+    print(estimate_sun_vec(pudding90))
 
 if __name__ == "__main__":
     main()
